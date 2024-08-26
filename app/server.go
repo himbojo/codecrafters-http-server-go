a561f843af85f748499817be6f6b904f6f6f72c4
HimboJo <jordan.akroyd@gmail.com>
2024-08-26 22:36:40 +1200

    Add HTTP target handling

--- app/server.go
+++ app/server.go
@@ -1,77 +1,177 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
+	"time"
 )
 
-type StatusLine struct {
+var routes = []string{"/", "/home", "/about"}
+
+type ResponseStatusLine struct {
 	HTTPVersion          string
 	StatusCode           int
 	OptionalReasonPhrase string
 }
 
-func NewStatusLine(httpVersion string, statusCode int, optionalReponsePhrase string) StatusLine {
-	statusLine := StatusLine{
+func (statusLine ResponseStatusLine) ToString() string {
+	return strings.Join([]string{statusLine.HTTPVersion, strconv.Itoa(statusLine.StatusCode), statusLine.OptionalReasonPhrase}, " ") + "\r\n"
+}
+
+type RequestStatusLine struct {
+	HTTPMethod    string
+	RequestTarget string
+	HTTPVersion   string
+}
+
+type Request struct {
+	StatusLine RequestStatusLine
+	Headers    map[string]string
+	Body       []byte
+}
+
+// Error implements error.
+func (r Request) Error() string {
+	panic("unimplemented")
+}
+
+func handleRequest(conn net.Conn) (Request, error) {
+	reader := bufio.NewReader(conn)
+
+	// Read the request line
+	requestLineString, err := reader.ReadString('\n')
+	if err != nil {
+		return Request{}, fmt.Errorf("error reading request line: %w", err)
+	}
+	// declare and print request line
+	requestLineArray := strings.Split(requestLineString, " ")
+	requestLine := RequestStatusLine{
+		HTTPMethod:    requestLineArray[0],
+		RequestTarget: requestLineArray[1],
+		HTTPVersion:   requestLineArray[2],
+	}
+
+	fmt.Println("\nRequest Line:")
+	fmt.Println("HTTP Method:", requestLine.HTTPMethod)
+	fmt.Println("Request Target:", requestLine.RequestTarget)
+	fmt.Println("HTTP Version:", requestLine.HTTPVersion)
+
+	// Read headers until the last \r\n
+	headerMap := make(map[string]string)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			return Request{}, fmt.Errorf("error reading headers: %w", err)
+		}
+		if line == "\r\n" {
+			break
+		}
+		parts := strings.SplitN(line, " ", 2)
+		key := parts[0][:len(parts[0])-1]
+		value := parts[1]
+		headerMap[key] = value
+	}
+
+	// print all header key value pairs
+	fmt.Println("Header:")
+	for key, value := range headerMap {
+		fmt.Printf("%s: %s", key, value)
+	}
+
+	// Read the rest of the data into the body
+	// add timeout as often no EOF
+	conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+	var body []byte
+	buffer := make([]byte, 1024)
+	for {
+		n := 0
+		n, err = conn.Read(buffer)
+		if err != nil {
+			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+				err = fmt.Errorf("read timeout: %w", err)
+				break
+			}
+			err = fmt.Errorf("read error: %w", err)
+			break
+		}
+		if n > 0 {
+			body = append(body, buffer[:n]...)
+		}
+	}
+	if err != nil {
+		fmt.Println("\nError reading data:", err)
+	}
+
+	// print the body if available
+	fmt.Println("\nBody:")
+	fmt.Print(string(body))
+
+	request := Request{
+		StatusLine: requestLine,
+		Headers:    headerMap,
+		Body:       body,
+	}
+
+	return request, nil
+}
+
+func handleResponse(conn net.Conn, request Request) error {
+	// Status Line
+	responseStatusLine := ResponseStatusLine{
 		HTTPVersion:          "HTTP/1.1",
 		StatusCode:           200,
 		OptionalReasonPhrase: "OK",
 	}
 
-	if httpVersion != "" {
-		statusLine.HTTPVersion = httpVersion
+	// Check if target is in route
+	if !slices.Contains(routes, request.StatusLine.RequestTarget) {
+		responseStatusLine.StatusCode = 404
+		responseStatusLine.OptionalReasonPhrase = "Not Found"
 	}
-
-	if statusCode != 0 {
-		statusLine.StatusCode = statusCode
-	}
-
-	if optionalReponsePhrase != "" {
-		statusLine.OptionalReasonPhrase = optionalReponsePhrase
-	}
-
-	return statusLine
-}
-
-func (statusLine StatusLine) ToString() string {
-	// if statusLine.OptionalReasonPhrase != "" {
-	return strings.Join([]string{statusLine.HTTPVersion, strconv.Itoa(statusLine.StatusCode), statusLine.OptionalReasonPhrase}, " ") + "\r\n"
-	// } else {
-	// 	return strings.Join([]string{statusLine.HTTPVersion, strconv.Itoa(statusLine.StatusCode)}, " ") + "\r\n"
-	// }
-
-}
-
-func handleConnection(conn net.Conn) {
-	fmt.Println("Remote:", conn.RemoteAddr(), "to Local:", conn.LocalAddr().String(), "via:", conn.LocalAddr().Network())
-	// Status Line
-	status := NewStatusLine("", 0, "").ToString()
-
 	// Headers
 	headers := "\r\n"
-
-	// Respons Body
-	body := ""
-
-	httpResponse := strings.Join([]string{status, headers, body}, "")
-	fmt.Printf("%q\n", httpResponse)
+	// Response Body
+	responseBody := ""
+	httpResponse := strings.Join([]string{responseStatusLine.ToString(), headers, responseBody}, "")
+	fmt.Printf("\nResponse:\n%q\n", httpResponse)
 
 	httpResponseBytes := []byte(httpResponse)
 	_, err := conn.Write(httpResponseBytes)
+	if err != nil {
+		return fmt.Errorf("read timeout: %w", err)
+	}
+	return nil
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("Remote:", conn.RemoteAddr())
+	fmt.Println("Local:", conn.LocalAddr().String())
+	fmt.Println("Protocol:", conn.LocalAddr().Network())
+
+	request, err := handleRequest(conn)
+	if err != nil {
+		fmt.Println("Error handling request: ", err.Error())
+		os.Exit(1)
+	}
+
+	err = handleResponse(conn, request)
+
 	if err != nil {
 		fmt.Println("Error writing response: ", err.Error())
 		os.Exit(1)
 	}
 
-	// err = conn.Close()
-	// if err != nil {
-	// 	fmt.Println("Error closing connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
-
+	err = conn.Close()
+	if err != nil {
+		fmt.Println("Error closing connection: ", err.Error())
+		os.Exit(1)
+	}
 }
 
 func main() {
